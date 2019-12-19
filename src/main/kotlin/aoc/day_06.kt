02e4b331c7fc9bbb2cbfecdd7245d34aac502442
jgoerner <joshua.goerner@gmail.com>
2019-12-19 22:48:49 +0100

    Finished Day 06

--- src/main/kotlin/aoc/day_06.kt
+++ src/main/kotlin/aoc/day_06.kt
@@ -47,6 +47,61 @@
  * The total number of direct and indirect orbits in this example is 42.
  *
  * What is the total number of direct and indirect orbits in your map data?
+ *
+ *
+ * --- Part Two ---
+ *
+ *
+ * Now, you just need to figure out how many orbital transfers you (YOU) need to take to get to Santa (SAN).
+ *
+ * You start at the object YOU are orbiting; your destination is the object SAN is orbiting. An orbital transfer lets
+ * you move from any object to an object orbiting or orbited by that object.
+ *
+ * For example, suppose you have the following map:
+ *
+ * COM)B
+ * B)C
+ * C)D
+ * D)E
+ * E)F
+ * B)G
+ * G)H
+ * D)I
+ * E)J
+ * J)K
+ * K)L
+ * K)YOU
+ * I)SAN
+ * Visually, the above map of orbits looks like this:
+ *
+ *                            YOU
+ *                           /
+ *          G - H       J - K - L
+ *        /           /
+ * COM - B - C - D - E - F
+ *                \
+ *                I - SAN
+ *
+ * In this example, YOU are in orbit around K, and SAN is in orbit around I. To move from K to I, a minimum of 4 orbital
+ * transfers are required:
+ *
+ * K to J
+ * J to E
+ * E to D
+ * D to I
+ *
+ * Afterward, the map of orbits looks like this:
+ *
+ *         G - H       J - K - L
+ *        /           /
+ * COM - B - C - D - E - F
+ *               \
+ *               I - SAN
+ *                \
+ *                YOU
+ *
+ * What is the minimum number of orbital transfers required to move from the object YOU are orbiting to the object SAN
+ * is orbiting? (Between the objects they are orbiting - not between YOU and SAN.)
  */
 
 package aoc
@@ -54,15 +109,24 @@ package aoc
 import java.io.File
 import kotlin.system.measureTimeMillis
 
+/**
+ * Map holding information about relationships between [Planet]
+ */
 class SpaceMap {
 
     private val _planets = mutableMapOf<Planet, Planet>()
 
+    /**
+     * Parses [input] and links planets
+     */
     fun parseOrbit(input: String) {
         val (center, orbit) = input.split(")").map { Planet(it) }
         _planets.putIfAbsent(orbit, center)
     }
 
+    /**
+     * Returns the number of direct and indirect orbits
+     */
     fun totalOrbits() : Int {
         var orbitCounter = 0
 
@@ -77,19 +141,55 @@ class SpaceMap {
         return orbitCounter
     }
 
+    /**
+     * Calculates the path from a given [input] to the "root" [Planet]
+     */
+    fun pathToRoot(input: Planet) : List<Planet>{
+        val path = mutableListOf(input)
+        var current: Planet = input.copy()
+        while (_planets[current] != null) {
+            path.add(_planets[current]!!)
+            current = _planets[current]!!
+        }
+        return path
+    }
+
+    /**
+     * Calculates the steps between [from] and [to]
+     */
+    fun stepsBetween(from: Planet, to: Planet): Int {
+        val pathFrom = pathToRoot(from).toMutableList()
+        val pathTo = pathToRoot(to).toMutableList()
+        pathFrom.reversed()
+                .zip(pathTo.reversed())
+                .forEach { (a, b) ->  if (a == b) { pathFrom.remove(a); pathTo.remove(b) }
+                }
+        return pathFrom.size + pathTo.size - 2
+    }
 }
 
+/**
+ * Planet identified by an [id]
+ */
 data class Planet(val id: String)
 
+/**
+ * Applies the [SpaceMap.totalOrbits] and [SpaceMap.stepsBetween] functions to the given input
+ */
 fun main() {
     val executionTime = measureTimeMillis {
 
-        // Part One
+        // General Setup
         val spaceMap = SpaceMap()
         File("src/main/resources/day06/input.txt")
                 .readLines()
                 .forEach { spaceMap.parseOrbit(it) }
+
+        // Part One
         println("Found ${spaceMap.totalOrbits()} orbits in total")
+
+        // Part Two
+        println("You are ${spaceMap.stepsBetween(Planet("YOU"), Planet("SAN"))} steps apart from Santa")
 
     }
     println("\n[elapsed time: $executionTime ms]")
--- src/test/kotlin/aoc/Day06Test.kt
+++ src/test/kotlin/aoc/Day06Test.kt
@@ -1,6 +1,5 @@
 package aoc
 
-import com.sun.imageio.plugins.jpeg.JPEG.COM
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -63,4 +62,69 @@ class Day06Test {
         assertEquals(expected, actual)
     }
 
+    @Test
+    fun `should find path to root`(){
+        // given
+        val input = listOf(
+                "A)B",
+                "B)C",
+                "B)D",
+                "C)E")
+        val expected = listOf("E", "C", "B", "A").map { Planet(it) }
+
+        // when
+        val spaceMap = SpaceMap()
+        input.forEach { spaceMap.parseOrbit(it) }
+        val actual = spaceMap.pathToRoot(Planet("E"))
+
+        // then
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should find simple path length between planets`(){
+        // given
+        val input = listOf(
+                "A)B",
+                "B)C",
+                "B)D",
+                "C)E")
+        val expected = 1
+
+        // when
+        val spaceMap = SpaceMap()
+        input.forEach { spaceMap.parseOrbit(it) }
+        val actual = spaceMap.stepsBetween(Planet("E"), Planet("D"))
+
+        // then
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should find advanced path length between planets`(){
+        // given
+        val input = listOf(
+                "COM)B",
+                "B)C",
+                "C)D",
+                "D)E",
+                "E)F",
+                "B)G",
+                "G)H",
+                "D)I",
+                "E)J",
+                "J)K",
+                "K)L",
+                "K)YOU",
+                "I)SAN")
+        val expected = 4
+
+        // when
+        val spaceMap = SpaceMap()
+        input.forEach { spaceMap.parseOrbit(it) }
+        val actual = spaceMap.stepsBetween(Planet("YOU"), Planet("SAN"))
+
+        // then
+        assertEquals(expected, actual)
+    }
 }
